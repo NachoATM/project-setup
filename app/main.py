908ab5896dc14c7104bb4edb7188d3f82b1a96cd
Jose Ignacio Esteban González <98879159+NachoATM@users.noreply.github.com>
2025-01-13 19:08:39 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from app.authentication.router import router as router_auth
+from app.files.router import router as router_files
 
 app = FastAPI()
 
 @app.get("/healthcheck")
 async def healthcheck() -> dict[str, str]:
     return {"status": "ok"}
+
+app.include_router(router_auth, prefix="")
+app.include_router(router_files, prefix="/files")
+
